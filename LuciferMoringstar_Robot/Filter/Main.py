459e9f8781b3eb1231a3b8bdb2eb9e9267fa1b32
PrinceStarLord <86295965+PrinceStarLord@users.noreply.github.com>
2022-08-16 04:04:58 -0500

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**Query:** query \n\n**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}**"
+        mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}**"
         files = await get_filter_results(query=search)
         if files:
             for file in files:
@@ -115,7 +115,7 @@ async def group(client, message):
     if 2 < len(message.text) < 50:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**Query:** query \n\n**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}**"
+        mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}**"
         nyva=BOT.get("username")
         if not nyva:
             botusername=await client.get_me()
@@ -275,24 +275,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 )
                 return
         elif query.data == "help":
-            buttons = [[
-                  InlineKeyboardButton('🎭 Updates', url='https://t.me/M2LINKS'),
-                  InlineKeyboardButton('🏠 Home', callback_data='start')
-                      ],
-                [
-                    InlineKeyboardButton('🏠 Close', callback_data='close_data')
-                ]
+            buttons = [
+                  InlineKeyboardButton('🎭 Updates', url='https://t.me/M2LINKS')
+                 
                   ]
             await query.message.edit(text=f"{HELP}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
         elif query.data == "about":
-            buttons = [[
-                  InlineKeyboardButton('🎭 Updates', url='https://t.me/M2LINKS'),
-                  InlineKeyboardButton('🏠 Home', callback_data='start')
-                      ],
-                      [
-                  InlineKeyboardButton('🏠 Close', callback_data='close_data')
-                     ]]
+            buttons = [
+                  InlineKeyboardButton('🎭 Updates', url='https://t.me/M2LINKS')
+                  ]
             
             await query.message.edit(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
