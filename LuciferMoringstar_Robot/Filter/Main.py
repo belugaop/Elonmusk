cdbf162dc132b192609b2ce64a28d568c4ca3e98
PrinceStarLord <86295965+PrinceStarLord@users.noreply.github.com>
2022-08-16 04:45:37 -0500

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -10,7 +10,7 @@ import random
 BUTTONS = {}
 BOT = {}
 
-@Client.on_message((filters.group | filters.private) & filters.text & ~filters.edited & filters.incoming & filters.user(AUTH_USERS)) if AUTH_USERS else filters.text & filters.private & filters.incoming)
+@Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming)
 async def filter(client, message):
     if message.text.startswith("/"):
        return 
