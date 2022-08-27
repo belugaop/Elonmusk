768ee28e6a5326a2cc0fdf1a746056061ee261f3
BELUGA_XD <96910602+belugaop@users.noreply.github.com>
2022-08-27 20:42:19 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -128,13 +128,7 @@ async def group(client, message):
                 filename = f"[{get_size(file.file_size)}] {file.file_name}"
                 btn.append(
 
-                    [InlineKeyboardButton(text=f"{filename}", url=f"https://api.shareus.in/directLink?token=
-
-xGzniHOkadfY4aP4HKbnGxHGGkv1
-
-&link=https://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id}")]
-
-                )
+                    [InlineKeyboardButton(text=f"{filename}", url=f"https://api.shareus.in/directLink?token=xGzniHOkadfY4aP4HKbnGxHGGkv1&link=https://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id}")]  )
         else:
             return
         if not btn:
